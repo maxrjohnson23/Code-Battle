dcf37a7aa93235d86f67368dade87319260ca874
Max Johnson <maxrjohnson23@gmail.com>
2018-04-21 14:37:12 -0500

    Fix merge with chat

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,55 @@ import CodeSpace from "./containers/CodeSpace/CodeSpace";
 import SignUp from "./components/SignUp/SignUp";
 import LoginForm from "./components/LoginForm/LoginForm";
 import Navbar from "./components/Navbar/Navbar";
+import LiveChat from "./components/LiveChat";
+import ChatHistory from "./components/ChatHistory";
+import PubNubReact from "pubnub-react";
 
 
 class App extends Component {
   state = {
     loggedIn: false,
-    username: null
+    username: null,
+    userID: Math.round(Math.random() * 1000000).toString(),
+    messages: []
   };
+
+  constructor(props) {
+    super(props);
+    this.pubnub = new PubNubReact({
+      publishKey: "pub-c-f890eb61-09b5-49e5-bed1-274a32208c3b",
+      subscribeKey: "sub-c-c6147708-458f-11e8-9967-869954283fb4"
+    });
+    this.pubnub.init(this);
+  }
+
+  componentWillMount() {
+    console.log(this.pubnub);
+    this.pubnub.subscribe({
+      channels: ["channel1"],
+      withPresence: true
+    });
+
+    this.pubnub.getMessage("channel1", (msg) => {
+      console.log(msg);
+    });
+  };
+
+  componentWillUnmount() {
+    this.pubnub.unsubscribe({
+      channels: ["channel1"]
+    });
+  };
+
+  sendMessage = (message) => {
+    this.pubnub.publish({
+      channel: "channel1",
+      message: message,
+    });
+    console.log(message);
+  };
+
+
 
   componentDidMount() {
     if (!this.state.loggedIn) {
@@ -46,6 +88,10 @@ class App extends Component {
 
 
   render() {
+    const messages = this.pubnub.getMessage("channel1");
+    console.log(messages);
+    const {sendMessage, state} = this;
+
     return (
         <div className="App">
           <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn}
@@ -55,6 +101,8 @@ class App extends Component {
                  render={() => <LoginForm updateUser={this.updateUser}/>}/>
           <Route path="/signup"
                  render={() => <SignUp/>}/>
+          <ChatHistory history={messages}/>
+          <LiveChat userID={state.userID} sendMessage={sendMessage}/>
         </div>
     );
   }
