5fb9e590d908f771d8c12aac92b71c91961e0db3
Guy Accettura <gaccettu@indiana.edu>
2018-04-21 11:26:17 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import AceEditor from "react-ace";
 import LiveChat from "./components/LiveChat";
-import ChatHistory from "./components/ChatHistory"
+import ChatHistory from "./components/ChatHistory";
+import PubNubReact from 'pubnub-react';
 
 import "brace/mode/javascript";
 import "brace/theme/github";
@@ -12,17 +13,38 @@ import 'brace/theme/monokai';
 
 class App extends Component {
 
-  componentDidMount() {
-    this.PubNub = PUBNUB.init({
-      publish_key: 'pub-c-fb92c27e-1e14-4842-8fda-17984b453dae',
-      subscribe_key: 'sub-c-94b0177a-3ffa-11e8-8ce7-1294c71dad07',
-      ssl: (location.protocol.toLowerCase() === 'https:'),
+  constructor(props){
+    super(props);
+    this.pubnub = new PubNubReact({ publishKey: 'pub-c-fb92c27e-1e14-4842-8fda-17984b453dae', subscribeKey: 'sub-c-94b0177a-3ffa-11e8-8ce7-1294c71dad07' });
+    this.pubnub.init(this);
+
+  }
+
+  componentWillMount() {
+
+    this.pubnub.subscribe({ 
+      channels: ['channel1'], 
+      withPresence: true, 
+      message: (message) => this.setState({ 
+      history: this.state.history.concat(message)}) 
     });
-    this.PubNub.subscribe({
-      channel: 'ReactChat',
+
+    //this.pubnub.getStatus();
+    //this.pubnub.getMessage('channel1');
+    this.pubnub.getPresence('channel1');
+    this.pubnub.getMessage('channel1');
+
+    /*this.pubnub.subscribe({
+      channel: 'channel1',
+      withPrescence: true,
       message: (message) => this.setState({ 
         history: this.state.history.concat(message) 
       }),
+    });*/
+     
+    this.pubnub.getMessage('channel1', (message) => {
+      console.log("hello");
+      console.log(message);
     });
   }
 
@@ -32,10 +54,11 @@ class App extends Component {
   };
   
   sendMessage = (message) => {
-    this.PubNub.publish({
-      channel: 'ReactChat',
+    this.pubnub.publish({
+      channel: 'channel1',
       message: message,
-    });
+      });
+      console.log(message);
   }
 
   onChange = (newValue) => {
