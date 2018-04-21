1dda2771e56b4ed6620145c2cd54364d27e5e071
Max Johnson <maxrjohnson23@gmail.com>
2018-04-21 12:07:07 -0500

    Add passport login functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "spectre.css/dist/spectre.min.css";
+import "spectre.css/dist/spectre-icons.css";
+import "./index.css";
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
 import "font-awesome/css/font-awesome.min.css";
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter>
+  <App/>
+</BrowserRouter>, document.getElementById("root"));
 registerServiceWorker();
--- server.js
+++ server.js
@@ -1,25 +1,58 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const session = require("express-session");
+const dbConnection = require("./db");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("./passport");
+const user = require("./routes/user");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// MIDDLEWARE
+app.use(morgan("dev"));
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+);
+app.use(bodyParser.json());
+
+// Sessions
+app.use(
+    session({
+      secret: "rando string", //pick a random string to make the hash that is generated secure
+      store: new MongoStore({mongooseConnection: dbConnection}),
+      resave: false, //required
+      saveUninitialized: false //required
+    })
+);
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session()); // calls the deserializeUser
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Routes
+app.use("/user", user);
+
 app.get("/api/test", (req, res) => {
   res.json({test: "TEST"});
-} );
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
