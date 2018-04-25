14d084eb7dc6830ff45c927d1a79db157446a66e
Max Johnson <maxrjohnson23@gmail.com>
2018-04-24 20:02:22 -0500

    Update mongo url for heroku

--- db/index.js
+++ db/index.js
@@ -4,9 +4,10 @@ mongoose.Promise = global.Promise;
 
 //your local database url
 //27017 is the default mongoDB port
-const uri = "mongodb://localhost:27017/code-battle";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/code-battle";
 
-mongoose.connect(uri).then(
+
+mongoose.connect(MONGODB_URI).then(
     () => {
       /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
       console.log("Connected to Mongo");
