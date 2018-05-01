85e1281b7343eec7100534c166ade50645162736
Max Johnson <maxrjohnson23@gmail.com>
2018-05-01 08:38:01 -0500

    Add route for getting random question and adding to published game details

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -8,9 +8,48 @@ router.get("/", (req, res) => {
         if (data) {
           res.json({questions: data});
         }
-      }).catch(err => {
-    res.status(500).send(err);
+      }).catch(() => {
+    res.status(500).send({error: "Error while retrieving question"});
   });
+});
+
+router.get("/:id", (req, res) => {
+  // Get random question
+  if (req.params.id === "random") {
+    // Get the count of all questions
+    db.Question.count().then((count) => {
+      // Get a random entry
+      const random = Math.floor(Math.random() * count);
+      db.Question.findOne().skip(random).then(
+          (result) => {
+            if (result) {
+              res.json({question: result});
+            } else {
+              res.status(404).send({error: "No questions found"});
+            }
+          });
+    }).catch(() => {
+      res.status(500).send({error: "Error while retrieving question"});
+
+    });
+  } else {
+    // find question by id
+    db.Question.findById(req.params.id)
+        .then(result => {
+          if (result) {
+            res.json({question: result});
+          } else {
+            res.status(404).send({error: "Question not found"});
+          }
+        }).catch(() => {
+      res.status(500).send({error: "Error while retrieving question"});
+    });
+  }
+});
+
+
+router.get("/random-question", (req, res) => {
+
 });
 
 router.post("/", (req, res) => {
